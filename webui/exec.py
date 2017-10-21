9c072753d8617836762b49e1fd6d1f87954302fb
航　三島 <w.mishima11@gmail.com>
2017-10-21 14:10:39 +0900

    display database contents

--- webui/exec.py
+++ webui/exec.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
 	contents = WikiContent.query.all()
-	return render_template("index.html", contents=contents)
+	return render_template("index.html", contents = contents)
 
 if __name__ == "__main__":
 	app.run()
