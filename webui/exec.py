dd6ef5f805502c65010fb1276ea1a453cb17b1fb
航　三島 <w.mishima11@gmail.com>
2017-10-21 13:51:17 +0900

    make html file

--- webui/exec.py
+++ webui/exec.py
@@ -1,11 +1,15 @@
-#coding:utf-8
+# -*- coding:utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flaski.models import WikiContent
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route('/')
+@app.route("/")
 def index():
-	return render_template("index.html")
+	contents = WikiContent.query.all()
+	return render_template("index.html", contents=contents)
 
 if __name__ == "__main__":
-		app.run(debug=True)
+	app.run()
