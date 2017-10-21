42602e4eb3383cae7091420f6dd7e5dae4d02818
航　三島 <w.mishima11@gmail.com>
2017-10-22 01:28:42 +0900

    test

--- webui/app.py
+++ webui/app.py
@@ -3,16 +3,19 @@
 import komagen_sd_client
 from flask import Flask, render_template, abort
 from flaski.models import WikiContent
-import komagen_sd_client,py
+import komates as koma
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-	#komagen = komagen_sd_client.komagen()
 	contents = WikiContent.query.all()
 	return render_template("index.html", contents = contents)
+
+@app.route("/", methods=["POST"])
+def callkomagen():
+	koma.komagen()
 
 if __name__ == "__main__":
 	app.run()
