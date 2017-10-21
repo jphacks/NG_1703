4bfee51cd06cc470c424ee526d997c249f892a1c
航　三島 <w.mishima11@gmail.com>
2017-10-21 22:35:28 +0900

    resize content

--- webui/exec.py
+++ webui/exec.py
@@ -3,12 +3,14 @@
 import komagen_sd_client
 from flask import Flask, render_template, abort
 from flaski.models import WikiContent
+import komagen_sd_client,py
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
+	#komagen = komagen_sd_client.komagen()
 	contents = WikiContent.query.all()
 	return render_template("index.html", contents = contents)
 
